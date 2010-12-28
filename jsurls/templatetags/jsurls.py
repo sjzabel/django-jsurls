eba63d7e0027cb54b64fbe97d3d9618719467cbd
Stephen Zabel <szabel@q2software.com>
2010-12-28 11:33:00 -0600

    Could not figure out any other way around the staticfiles_urlpatterns problem, so I created a template tag to get around the issue

---
+++ jsurls/templatetags/jsurls.py
@@ -0,0 +1,18 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+@register.tag(name="get_jsurls_static_prefix")
+def do_render_jsurls_static_prefix(parser,token):
+    return RenderStaticPrefixNode()
+
+class RenderStaticPrefixNode(template.Node):
+    def render(self,context):
+        if settings.DEBUG:
+            return '/static_js_urls/'
+        else:
+            if 'STATIC_PREFIX' in context:
+                return context["STATIC_PREFIX"]
+            else:
+                return ''
+
